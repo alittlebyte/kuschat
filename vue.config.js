76886d94d556c9253271ff94298c1e6ca2913b97
Vlad <50201776+alittlebyte@users.noreply.github.com>
2020-01-06 05:14:31 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/kustalk/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/kuschat/' : '/',
   "transpileDependencies": [
     "vuetify"
   ]
